8f70c9af312ecdbc0c071437f7e25adfc33af8a1
jsb <tjdqls2021@gmail.com>
2023-11-20 15:27:00 +0900

    파일 업로드 기능

--- src/main/java/com/example/prj1be20231109/service/BoardService.java
+++ src/main/java/com/example/prj1be20231109/service/BoardService.java
@@ -8,9 +8,15 @@ import com.example.prj1be20231109.mapper.CommentMapper;
 import com.example.prj1be20231109.mapper.FileMapper;
 import com.example.prj1be20231109.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,6 +35,11 @@ public class BoardService {
     private final LikeMapper likeMapper;
     private final FileMapper fileMapper;
 
+    @Value("${aw3.s3.bucket.name}")
+    private   String bucket;
+
+
+    private final S3Client s3;
 
     public boolean save(Board board, MultipartFile[] files, Member login) throws IOException {
         //
@@ -53,16 +64,17 @@ public class BoardService {
     private void upload(Integer boardId, MultipartFile file) throws IOException {
         // 파일 저장 경로
         // c:\Temp\prj1\게시물번호\파일명
+        String key = "prj1/" + boardId + "/" + file.getOriginalFilename();
+//
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .acl(ObjectCannedACL.PUBLIC_READ)
+                        .build();
+
+        s3.putObject(objectRequest, RequestBody.fromInputStream(file.getInputStream(),file.getSize()));
 
 
-           File folder = new File("c:\\Temp\\prj1\\"+boardId);
-           if(!folder.exists()) {
-               folder.mkdirs();
-
-               String path = folder.getAbsolutePath() + "\\" + file.getOriginalFilename();
-               File des = new File(path);
-               file.transferTo(des);
-           }
     }
 
     public boolean validate(Board board) {
