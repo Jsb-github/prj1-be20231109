c266b9413341883c3716d24ea75d395655ed7a7c
jsb <tjdqls2021@gmail.com>
2023-11-22 14:43:42 +0900

    404 응답 처리를 위한 AppErrorController 추가

--- src/main/java/com/example/prj1be20231109/controller/AppErrorController.java
+++ src/main/java/com/example/prj1be20231109/controller/AppErrorController.java
@@ -3,7 +3,6 @@ package com.example.prj1be20231109.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class AppErrorController implements ErrorController {
